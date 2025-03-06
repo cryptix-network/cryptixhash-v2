04332cd499b5c850432c87dfb446cff68c7a566f
Cryptix Network <info@king-krypto.de>
2025-03-06 10:30:37 +0100

    Update beta.rs

--- beta.rs
+++ beta.rs
@@ -121,7 +121,7 @@ fn s_box_2(value: u8) -> Result<u8, &'static str> {
         0x56, 0x7A, 0x3C, 0x31, 0x79, 0x41, 0xD9, 0xC1, 0xF3, 0x8E, 0x62, 0xC9, 0xD3, 0x6E, 0x45, 0x6A,
     ];
 
-    // Check if the value is within the valid range
+    
     if value > 0x1F {
         return Err("Value for s_box_2 is out of valid range");
     }
@@ -135,7 +135,7 @@ fn s_box_3(value: u8) -> Result<u8, &'static str> {
         0xF6, 0x4E, 0x81, 0x95, 0xC0, 0x76, 0x83, 0x4C, 0xBE, 0x7B, 0x6B, 0xD3, 0x38, 0x45, 0xB3, 0x92,
     ];
 
-    // Check if the value is within the valid range
+    
     if value > 0x1F {
         return Err("Value for s_box_3 is out of valid range");
     }
@@ -149,7 +149,7 @@ fn s_box_4(value: u8) -> Result<u8, &'static str> {
         0x8D, 0x39, 0x51, 0xAD, 0x8A, 0xF1, 0x69, 0x68, 0x29, 0x11, 0x64, 0x9C, 0x99, 0xC8, 0x54, 0x46,
     ];
 
-    // Check if the value is within the valid range
+    
     if value > 0x1F {
         return Err("Value for s_box_4 is out of valid range");
     }
