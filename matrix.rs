2b757127fb481c4ffa854c03a97489e5e8b18362
Cryptix Network <info@king-krypto.de>
2025-03-13 11:37:59 +0100

    Update matrix.rs

--- matrix.rs
+++ matrix.rs
@@ -101,6 +101,7 @@ impl Matrix {
     // TODO:
     // Add a nonlinear Cache Function, should fits to L1, L2 Cache
     // Rewrite Memory Hard Function to a real Memory Hard Function (its to easy now)
+    // Consider bottlenecking the LUTS with 64-bit values, which would overload at least 1,000,000 LUTs. But this needs to be considered first. The current LUT usage must also be calculated with Vivado Studio.
 
     // Const Final Cryptix
     const FINAL_CRYPTIX: [u8; 32] = [
