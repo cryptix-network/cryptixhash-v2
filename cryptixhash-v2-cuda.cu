76ac77e3765b204e20209bd2d4ac69b946dcb47b
Cryptix Network <info@king-krypto.de>
2025-03-25 17:23:34 +0100

    Update cryptixhash-v2-cuda.cu

--- cryptixhash-v2-cuda.cu
+++ cryptixhash-v2-cuda.cu
@@ -104,15 +104,31 @@ extern "C" {
                 sha3_hash[i] = hash_.hash[i];
             }
             
-            // SHA3 Iterationen basierend auf dem ersten Byte
+            // SHA3 from first byte
             for (uint8_t i = 0; i < iteration_count; ++i) {
                 sha3(sha3_hash, 32, sha3_hash, 32);
 
                 // Condition
                 if (sha3_hash[3] % 3 == 0) { 
                     sha3_hash[20] ^= 0x55; 
-                } else if (sha3_hash[7] % 5 == 0) { 
+                } 
+                else if (sha3_hash[7] % 5 == 0) { 
                     sha3_hash[25] = rotate_left(sha3_hash[25], 7); 
+                } 
+                else if (sha3_hash[5] % 2 == 0) { 
+                    sha3_hash[10] ^= 0xAA; 
+                } 
+                else if (sha3_hash[6] % 4 == 0) {
+                    sha3_hash[15] = rotate_left(sha3_hash[15], 3); 
+                } 
+                else if (sha3_hash[8] % 7 == 0) {
+                    sha3_hash[30] ^= 0xFF; 
+                } 
+                else if (sha3_hash[9] % 11 == 0) {
+                    sha3_hash[5] = rotate_right(sha3_hash[5], 4); 
+                } 
+                else if (sha3_hash[12] % 13 == 0) {
+                    sha3_hash[18] = rotate_left(sha3_hash[18], 2); 
                 }
             }
 
